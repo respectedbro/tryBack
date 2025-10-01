cd68fc98c2831f1c17b69c68126d79dba3e613d9
NikDm <respectedoff@yandex.ru>
2025-10-01 19:57:02 +0300

    Endonists of removal and receipt

--- server/controllers/todosController.js
+++ server/controllers/todosController.js
@@ -3,6 +3,11 @@ const TodosModel = require("../models/todosModel");
 class TodosController {
   async getTodos(req, res) {
     try {
+      const result = await TodosModel.find({}, "title");
+      if (!result) {
+        res.status(400).json({ message: "Произошла ошибка при получении" });
+      }
+      res.status(200).json({ todos: result });
     } catch (e) {
       res.status(400).json({ message: "Произошла ошибка при получении" });
     }
@@ -26,6 +31,21 @@ class TodosController {
 
   async deleteTodo(req, res) {
     try {
+      if (!req.body.title) {
+        res.status(400).json({ message: "Пожалуйста укажите заголовок" });
+      }
+
+      const { deletedCount } = await TodosModel.deleteOne({
+        title: req.body.title,
+      });
+
+      if (deletedCount === 0) {
+        res
+          .status(400)
+          .json({ message: "Удаления не произошло, проверьте заголовок" });
+      }
+
+      res.status(200).json({ message: "Элемент был успешно удалён" });
     } catch (e) {
       res.status(400).json({ message: "Произошла ошибка при удалении" });
     }
